eb2f9b5f3c85f7932d66093556f77553fba9eda2
YRRING <050427@sjtu.edu.cn>
2025-07-17 12:40:03 +0800

    Update __init__.py

--- agents/ai_bots/__init__.py
+++ agents/ai_bots/__init__.py
@@ -7,11 +7,21 @@ from .minimax_bot import MinimaxBot
 from .mcts_bot import MCTSBot
 from .rl_bot import RLBot
 from .behavior_tree_bot import BehaviorTreeBot
+from .snake_ai import SnakeAI, SmartSnakeAI
+
+# 检查是否有推箱子AI
+try:
+    from .sokoban_ai import SokobanAI, SmartSokobanAI, ExpertSokobanAI
+    _sokoban_bots = ['SokobanAI', 'SmartSokobanAI', 'ExpertSokobanAI']
+except ImportError:
+    _sokoban_bots = []
 
 __all__ = [
     'RandomBot',
     'MinimaxBot',
     'MCTSBot',
     'RLBot',
-    'BehaviorTreeBot'
-] 
+    'BehaviorTreeBot',
+    'SnakeAI',
+    'SmartSnakeAI'
+] + _sokoban_bots
